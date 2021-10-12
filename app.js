a736f701800c5b4a07efc0f753ba4b039f47b8a8
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-12 13:36:35 -0400

    created html webpage

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
-// notice the lack of parentheses around the "profileDataArr" parameter?
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-  console.log("==========");
+const profileDataArgs = process.argv.slice(2); // creates an empty array
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile("./index.html", generatePage(name, github), err => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+
+
+
+
+// Module 9 - Chapter 1
+// console.log(profileDataArgs);
+
+// // notice the lack of parentheses around the "profileDataArr" parameter?
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+//   console.log("==========");
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
