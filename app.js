e93d1a66944dd9b0251dcc9b1868c6d6e4a292f5
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-12 10:12:34 -0400

    node testing

---
+++ app.js
@@ -0,0 +1,6 @@
+// notice the lack of parentheses around the "profileDataArr" parameter?
+const printProfileData = (profileDataArr) => {
+  for (let i = 0; i < profileDataArr.length; i++) {
+    console.log(profileDataArr);
+  }
+};
